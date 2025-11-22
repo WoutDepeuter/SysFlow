d444e3b5ffb45f5e27ae20a0f0d21ad813a9fbcd
WoutDepeuter <123627621+WoutDepeuter@users.noreply.github.com>
2025-11-22 15:22:04 +0100

    Refactor Show-Menu function for clarity and improve user interaction flow

--- SysFlow/Start-SysFlow.ps1
+++ SysFlow/Start-SysFlow.ps1
@@ -1,16 +1,6 @@
 
 
-function Show-Menu{
-    Write-Host "==========================" -ForegroundColor Cyan
-    Write-Host "     Sysflow              " -ForegroundColor Cyan
-    Write-Host "==========================" -ForegroundColor Cyan
-    Write-Host "1. System Status"
-    Write-Host "2. Back Ups"
-    Write-Host "3. Software Management"
-    Write-Host "4. Taskmanager"
-    Write-Host "5. Exit"    
-}
-
+#Function to show all menu options
 function Show-Menu {
     Write-Host "==========================" -ForegroundColor Cyan
     Write-Host "     Sysflow              " -ForegroundColor Cyan
@@ -21,14 +11,14 @@ function Show-Menu {
     Write-Host "4. Taskmanager"
     Write-Host "5. Exit"
 }
-
+#clear the host and show the menu
 Clear-Host
 Show-Menu
-
+#shows the menu until the users chooses to exit
 do {
     $keuze = Read-Host "Select an option (1-5)"
     Clear-Host
-
+    #switch to process the user's choice and open the correct module
     switch ($keuze) {
         '1' { Write-Host "Module Loaded: System Status" -ForegroundColor Green }
         '2' { Write-Host "Module Loaded: Back Ups" -ForegroundColor Green }
@@ -38,6 +28,7 @@ do {
         Default { Write-Host "Invalid Choice" -ForegroundColor Red }
     }
 
+    #if the user didn't choose to exit, wait for input and show the menu again
     if ($keuze -ne '5') {
         Write-Host ""
         Write-Host "Press Enter to go back to the menu"
@@ -46,4 +37,5 @@ do {
         Show-Menu
     }
 
+    #if the user chose to exit, the loop ends
 } until ($keuze -eq '5')
