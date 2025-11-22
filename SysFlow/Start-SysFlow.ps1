4f72502267d45b2023e5f5e62dc87941f1f631ea
WoutDepeuter <123627621+WoutDepeuter@users.noreply.github.com>
2025-11-22 10:23:33 +0100

    Add initial menu function to Start-SysFlow.ps1

---
+++ SysFlow/Start-SysFlow.ps1
@@ -0,0 +1,14 @@
+
+
+function Show-Menu{
+    Clear-Host
+    Write-Host "==========================" -ForegroundColor Cyan
+    Write-Host "     Sysflow              " -ForegroundColor Cyan
+    Write-Host "==========================" -ForegroundColor Cyan
+    Write-Host "1. System Status"
+    Write-Host "2. Back Ups"
+    Write-Host "3. Software Management"
+    Write-Host "4. Taskmanager"
+}
+
+Show-Menu
